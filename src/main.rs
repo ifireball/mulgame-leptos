4c9241aeea9f33c5448cbbec4822782fbe641ac2
Barak Korren <bkorren@redhat.com>
2025-10-19 18:46:48 +0300

    Try out macro-less syntax
    Signed-off-by: Barak Korren <bkorren@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use leptos::prelude::*;
+use leptos::html::*;
 
 fn main() {
-    leptos::mount::mount_to_body(|| view! { <p>"Hello, world!"</p> });
+    leptos::mount::mount_to_body(App);
+}
+
+fn App() -> impl IntoView {
+    (
+        h1().child("Hello, world!"),
+        p().child("This is a paragraph."),
+    )
 }
